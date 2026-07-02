3a61be2ba4299cbb49e2941c8fa0aedfce7481df
dasdevidutta3@gmail.com <dasdevidutta3@gmail.com>
2026-07-02 06:30:04 +0530

    new file

---
+++ Topic-1/cu_2_T_1_creating_arrays.py
@@ -0,0 +1,5 @@
+import numpy 
+#Arrays is nothing but collection of similar type of data stored in memory
+#variable_name=numpy.array([elements])
+arr=numpy.array([1,2,3,4])
+print(f"The Printed Array Is ✅:{arr} ")
